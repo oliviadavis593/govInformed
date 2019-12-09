0c62d766676a2342cd7e1cb10c2fd5da3b4db2b0
Olivia Davis <olivia.davis593@gmail.com>
2019-12-08 20:02:24 -0600

    created News form functions

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const fecAPI = 'uXkCebDReSWI3hEUDb7vgd08sNjNktwhLylH0MD9';
 const candidateURL = 'https://api.open.fec.gov/v1/candidates/';
+const newsAPI = 'b9e9c385246849c0b943c3902f635af9';
+const newsURL = 'https://newsapi.org/v2/top-headlines';
 
 //convert object 
 function formatQueryParams(params) {
@@ -9,6 +11,10 @@ function formatQueryParams(params) {
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
     return queryItems.join('&');
 }
+
+/*=====================================*/
+/* Candidate Form Functions ======= */
+/*=====================================*/
 
 //fetches candidate information from the FEC API (GET request)
 function fetchCandidateInfo(candidateName, yearInput, officeInput) {
@@ -20,9 +26,9 @@ function fetchCandidateInfo(candidateName, yearInput, officeInput) {
   };
 
     const queryString = formatQueryParams(params);
-    const url = candidateURL + '?' + queryString;
+    const firstURL = candidateURL + '?' + queryString;
 
-  fetch(url)
+  fetch(firstURL)
     .then(response => {
         if(response.ok) {
             return response.json()
@@ -55,10 +61,71 @@ function watchCandidateSubmit() {
     });
 }
 
-function displayResults(responseJson) {
+//Display candidate results to the dom 
+function candidateResults(responseJson) {
     console.log(responseJson);
+   $('.candidate-results').empty();
+    //iterate through items array 
+    let html = '';
+    for(let i = 0; i < responseJson.results.length; i += 1) {
+
+        
+    }
+
+
 }
 
 
+/*=====================================*/
+/* News Form Functions ======= */
+/*=====================================*/
+
+//Convert object
+function formatNewsParams(newsParams) {
+    const queryContent = Object.keys(newsParams)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(newsParams[key])}`);
+    return queryContent.join('&');
+}
+
+//GET request from news API
+function fetchNewsInfo(newsInput) {
+    const newsParams = {
+        apiKey: newsAPI,
+        q: newsInput
+    };
+
+    const queryStringTwo = formatNewsParams(newsParams);
+    const secondURL = newsURL + '?' + queryStringTwo;
+
+    fetch(secondURL) 
+    .then(response => {
+        if(response.ok) {
+            return response.json()
+        }
+        throw new Error(response.statusText)
+    })
+    .then(responseJson => console.log(responseJson))
+    .catch(error => {
+        $('#js-error-message').text(`Something went wrong: ${error.message}`);
+    });
+}
+
+//form submission for news form 
+function newsForm() {
+    $('.news-form').submit(e => {
+        e.preventDefault();
+        let newsInput = $('.news-input').val();
+        fetchNewsInfo(newsInput);
+    })
+}
+
+//submit button for news form 
+function watchNewsSubmit() {
+    $('.news-submit').submit(e => {
+        e.preventDefault();
+        fetchNewsInfo();
+    })
+}
 
 candidateForm();
+newsForm();
