ea2ab083c5f3b00fe9a8136cedc5aab5597cc267
Olivia Davis <olivia.davis593@gmail.com>
2019-12-15 20:10:31 -0600

    removed jQuery animations due to glitch

--- main.js
+++ main.js
@@ -192,13 +192,7 @@ function newsResults(responseJson) {
     $('#js-news').removeClass('hidden');
 }
 
-//jQuery fade effects
-$(function() {
-    $('#header-container h1').hide().fadeIn(2000);
-    $('#header-container p').hide().fadeIn(2000);
-    $('.candidate-form').hide().fadeIn(2000);
-    $('.news-form').hide().fadeIn(2000);
-});
+
 
 candidateForm();
 newsForm();
